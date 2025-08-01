e5d2fde55c600abe0ce6f85966d784df21a32dd7
RenatTest <renattesttask@gmail.com>
2025-08-01 10:31:22 +0300

    add comments

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -200,6 +200,8 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  // METHODS
+
   Future<void> launchUrlmail(String url) async {
     if (await canLaunchUrl(Uri.parse(url))) {
       await launchUrl(Uri.parse(url));
